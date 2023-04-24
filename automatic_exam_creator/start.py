fc219ff8525725989bab79234797eddb47ddc51c
Jeremy Josey <webjeremy@gmail.com>
2023-04-24 12:04:48 -0300

    finished quiz maker

--- automatic_exam_creator/start.py
+++ automatic_exam_creator/start.py
@@ -3,7 +3,7 @@ import openai
 
 openai.api_key = os.getenv('OPENAI_SECRET_KEY')
 
-num_questions = 5
+num_questions = 4
 num_possible_answers = 4
 
 # Prompt the user for the topic they want to create a quiz on.
@@ -16,8 +16,8 @@ def query_openai(prompt):
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
-        temperature=0.7,
-        max_tokens=300
+        temperature=0,
+        max_tokens=350
     )
     return response
 
@@ -42,6 +42,7 @@ def create_student_view(quiz, num_questions):
                 student_view[question_number] = ''
     return student_view
 
+#pulls out the answers from the quiz.
 def extract_answers(quiz, numofquestions):
     answers = {1: ''}
     question_number = 1
@@ -53,6 +54,24 @@ def extract_answers(quiz, numofquestions):
                 answers[question_number] = ''
     return answers
 
+# runs the test and gets answers.
+def take_test(student_view):
+    student_answers = {}
+    for question, question_text in student_view.items():
+        if(question_text == ''):
+            continue
+        print(question_text)
+        student_answers[question] = input("Enter your answer: ")
+    return student_answers
+
+def grade_test(student_answers, answers):
+    correct = 0
+    for question, answer in student_answers.items():
+        if answer.upper() == answers[question][1]:
+            correct += 1
+    grade = 100 * correct / len(student_answers)
+    return f"You got {correct} out of {len(student_answers)} questions correct. Your grade is {grade}%"
+
 
 # start the script
 topic = prompt_input()
@@ -60,11 +79,7 @@ prompt = create_test_prompt(topic, num_questions, num_possible_answers)
 results = query_openai(prompt)
 student_view = create_student_view(results['choices'][0]['text'], num_questions)
 answers = extract_answers(results['choices'][0]['text'], num_questions)
-
-for key in student_view:
-  print(student_view[key])
-
-for key in answers:
-  print(answers[key])
+student_answers = take_test(student_view)
+print(grade_test(student_answers, answers))
 
 
